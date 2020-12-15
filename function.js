da099af25a2261eaf3007809ca3e711ca5d03eb2
jinx244 <65917675+jinx244@users.noreply.github.com>
2020-12-15 12:04:23 +0100

    Add files via upload

--- function.js
+++ function.js
@@ -16,47 +16,32 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-/*
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlides(n)
-{
-    showSlides(slideIndex +=n);
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-function currentSlide(n)
-{
+// Thumbnail image controls
+function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-function showSlides(n)
-{
+function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var preview = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-
-    if (n > slides.length)
-    {slideIndex=1}
-    if (n < 1)
-    {slideIndex = slides.length}
-
-    for (i=0; i<slides.length; i++)
-    {slides[i].style.display = "none";}
-
-    for (i=0; i<preview.length; i++)
-    {preview[i].className = preview[i].className.replace(" active","");}
-
+    var dots = document.getElementsByClassName("demo");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
     slides[slideIndex-1].style.display = "block";
-    preview[slideIndex-1].className += " active";
-    captionText.innerHTML =preview[slideIndex-1].alt;
-
-}*/
-
-function myFunction(imgs) {
-    // Get the expanded image
-    var expandImg = document.getElementById("expandedImg");
-    expandImg.src = imgs.src;
-    expandImg.parentElement.style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
